b8b4ac87c1a5dd7a4382340579183ba4e8f1319a
patrick-peiliu <patrickliupack@gmail.com>
2024-09-11 16:47:17 +1200

    mobile debugging

--- src/main/java/com/product/api/config/CorsConfig.java
+++ src/main/java/com/product/api/config/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOriginPattern("http://127.0.0.1:5500"); // Allow specific test domain
+        corsConfiguration.addAllowedOriginPattern("http://192.168.68.74:5500"); // Allow specific test domain
         corsConfiguration.addAllowedOriginPattern("*epicsourcing*"); // Allow any domain containing "epicsourcing"
         corsConfiguration.addAllowedOriginPattern("*.webflow.io"); // Allow any test domain of Webflow
         corsConfiguration.addAllowedMethod("*"); // Allow all methods (GET, POST, etc.)
