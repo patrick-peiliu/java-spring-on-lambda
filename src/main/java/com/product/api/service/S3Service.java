1118bfdf4f3e3c685949bf485ca90be626252785
patrick-peiliu <patrickliupack@gmail.com>
2024-09-27 20:58:01 +1200

    fix: download the S3 file

--- src/main/java/com/product/api/service/S3Service.java
+++ src/main/java/com/product/api/service/S3Service.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.IOException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,33 +23,39 @@ public class S3Service {
     }
 
     public String uploadFile(MultipartFile file) throws Exception {
-        String timestamp = String.valueOf(System.currentTimeMillis());
-        String shortTimestamp = timestamp.substring(Math.max(timestamp.length() - 6, 0));
-        String originalFilename = "file";
-        if (StringUtils.isNotEmpty(file.getOriginalFilename())) {
-            originalFilename = file.getOriginalFilename();
+        try {
+            String timestamp = String.valueOf(System.currentTimeMillis());
+            String shortTimestamp = timestamp.substring(Math.max(timestamp.length() - 6, 0));
+            String originalFilename = "file";
+            if (StringUtils.isNotEmpty(file.getOriginalFilename())) {
+                originalFilename = file.getOriginalFilename();
+            }
+            String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String fileName = originalFilename.substring(0, originalFilename.lastIndexOf("."));
+
+            String filePath = fileName + "-" + shortTimestamp + fileExtension;
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+
+            // Set the appropriate Content-Type
+            String contentType = file.getContentType();
+            if (contentType == null || contentType.isEmpty()) {
+                contentType = "application/octet-stream";
+            }
+            metadata.setContentType(contentType);
+            metadata.setHeader("Content-Disposition", "attachment; filename=\"" + originalFilename + "\"");
+            // view in the browser
+            // Explicitly set Content-Disposition to inline
+//            metadata.setHeader("Content-Disposition", "inline");
+
+            PutObjectRequest request = new PutObjectRequest(bucketName, filePath, file.getInputStream(), metadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead);
+            amazonS3.putObject(request);
+
+            return amazonS3.getUrl(bucketName, filePath).toString();
+        } catch (IOException e) {
+            // Log the error and/or wrap it in a custom exception
+            throw new Exception("Failed to upload file to S3", e);
         }
-        String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
-        String fileName = originalFilename.substring(0, originalFilename.lastIndexOf("."));
-
-        String filePath = fileName + "-" + shortTimestamp + fileExtension;
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-
-        // Set the appropriate Content-Type
-        String contentType = file.getContentType();
-        if (contentType == null || contentType.isEmpty()) {
-            contentType = "application/octet-stream";
-        }
-        metadata.setContentType(contentType);
-
-        // Explicitly set Content-Disposition to inline
-        metadata.setHeader("Content-Disposition", "inline");
-
-        PutObjectRequest request = new PutObjectRequest(bucketName, filePath, file.getInputStream(), metadata)
-            .withCannedAcl(CannedAccessControlList.PublicRead);
-        amazonS3.putObject(request);
-
-        return amazonS3.getUrl(bucketName, filePath).toString();
     }
 }
