820f007bf0fb1a4fbd15f4af5749d5ecc8090783
patrick-peiliu <patrickliupack@gmail.com>
2024-10-01 12:27:16 +1300

    fix: S3 filename

--- src/main/java/com/product/api/service/S3Service.java
+++ src/main/java/com/product/api/service/S3Service.java
@@ -5,14 +5,20 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.IOException;
-import org.apache.commons.lang3.StringUtils;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+import org.apache.commons.io.FilenameUtils;
+import java.time.format.DateTimeFormatter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class S3Service {
-
+    private static final Logger LOG = LogManager.getLogger();
     private final AmazonS3 amazonS3;
 
     @Value("${aws.s3.bucket}")
@@ -24,16 +30,8 @@ public class S3Service {
 
     public String uploadFile(MultipartFile file) throws Exception {
         try {
-            String timestamp = String.valueOf(System.currentTimeMillis());
-            String shortTimestamp = timestamp.substring(Math.max(timestamp.length() - 6, 0));
-            String originalFilename = "file";
-            if (StringUtils.isNotEmpty(file.getOriginalFilename())) {
-                originalFilename = file.getOriginalFilename();
-            }
-            String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
-            String fileName = originalFilename.substring(0, originalFilename.lastIndexOf("."));
-
-            String filePath = fileName + "-" + shortTimestamp + fileExtension;
+            String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+            String uniqueFileName = generateUniqueFileName(fileExtension);
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentLength(file.getSize());
 
@@ -43,19 +41,29 @@ public class S3Service {
                 contentType = "application/octet-stream";
             }
             metadata.setContentType(contentType);
-            metadata.setHeader("Content-Disposition", "attachment; filename=\"" + originalFilename + "\"");
+            metadata.setHeader("Content-Disposition", "attachment; filename=\"" + uniqueFileName + "\"");
             // view in the browser
             // Explicitly set Content-Disposition to inline
 //            metadata.setHeader("Content-Disposition", "inline");
 
-            PutObjectRequest request = new PutObjectRequest(bucketName, filePath, file.getInputStream(), metadata)
+            PutObjectRequest request = new PutObjectRequest(bucketName, uniqueFileName, file.getInputStream(), metadata)
                 .withCannedAcl(CannedAccessControlList.PublicRead);
             amazonS3.putObject(request);
 
-            return amazonS3.getUrl(bucketName, filePath).toString();
+            String fileUrl = amazonS3.getUrl(bucketName, uniqueFileName).toString();
+            LOG.info("File successfully uploaded: fileName={}, url={}", uniqueFileName, fileUrl);
+            return fileUrl;
         } catch (IOException e) {
             // Log the error and/or wrap it in a custom exception
             throw new Exception("Failed to upload file to S3", e);
         }
+    }
+
+    private String generateUniqueFileName(String fileExtension) {
+        ZoneId aucklandZone = ZoneId.of("Pacific/Auckland");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+        String timestamp = ZonedDateTime.now(aucklandZone).format(formatter);
+        String uniqueId = UUID.randomUUID().toString().substring(0, 4);
+        return timestamp + "_" + uniqueId + "." + fileExtension;
     }
 }
